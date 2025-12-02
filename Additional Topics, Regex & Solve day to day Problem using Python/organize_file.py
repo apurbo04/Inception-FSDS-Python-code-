b9d2ba276827cca7cfc371a552ca59e920c75c59
apurbo04 <apurbasharma23@gmail.com>
2025-12-02 21:16:11 +0600

    New update

---
+++ Additional Topics, Regex & Solve day to day Problem using Python/organize_file.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+path = input ("Enter the path: ")
+files = os.listdir(path)
+# print(files)
+
+for file in files:
+    filename, extension = os.path.splitext(file)
+    # print(filename, extension)
+    extension = extension[1:]
+    # print(extension)
+    
+    if os.path.exists(path+"/"+extension):
+        shutil.move(path+"/"+file, path+"/"+extension+"/"+file)
+        
+    else:
+        os.makedirs(path+"/"+extension)
+        shutil.move(path+"/"+file, path+"/"+extension+"/"+file)
+        
